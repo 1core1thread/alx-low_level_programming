d30b2982618a64d91dcab6fa755a7faf1a596390
1code1thread <karim.sanhaji20@gmail.com>
2023-08-14 13:30:10 -0700

    no commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,7 @@
 #include<stdio.h>
 /**
- * main: is always the first function in C
- * puts: allows me to print a long chain of characters
- * return 0: it means that in the end I will get 0 in output
+ * Main: is always the first function in C
+ * return: it means that in the end I will get 0 in output
  */
 int main(void)
 {
