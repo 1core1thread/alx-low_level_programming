b7802aab62c187be46b896112dff0a06b6a9f22d
1code1thread <karim.sanhaji20@gmail.com>
2023-08-14 13:19:55 -0700

    correcting to betty style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,11 @@
 #include<stdio.h>
-int main(void) {
+/**
+ * main : is always the first function in C
+ * puts : allows me to print a long chaine of characters
+ * return0 : it means that in the end i will get 0 in output
+ */
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	return 0;
 }	
