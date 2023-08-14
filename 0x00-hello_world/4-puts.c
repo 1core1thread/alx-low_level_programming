c2e5f1b7943f1a560a77e2325d1158cc48aa4abb
1code1thread <karim.sanhaji20@gmail.com>
2023-08-14 13:26:30 -0700

    ok no spaces

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 /**
- * main : is always the first function in C
- * puts : allows me to print a long chaine of characters
- * return0 : it means that in the end i will get 0 in output
+ * main: is always the first function in C
+ * puts: allows me to print a long chain of characters
+ * return 0: it means that in the end I will get 0 in output
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
-}	
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
+}
